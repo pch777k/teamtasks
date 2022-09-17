5af5b96c6fd7aa5110668d752e64b22aa921c315
Pawel Chmiel-Kozdranski <pch777@gazeta.pl>
2022-09-17 14:37:42 +0200

    initial commit

---
+++ src/main/java/com/pch777/teamtasks/TeamTasksApplication.java
@@ -0,0 +1,13 @@
+package com.pch777.teamtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pch777/teamtasks/TeamTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pch777.teamtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
